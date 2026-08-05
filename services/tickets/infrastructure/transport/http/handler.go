70878feb0fd60f248af3bed706a4a4e69ec2a420
Chef <chefadekvat@gmail.com>
2026-08-05 19:53:30 +0300

    feat(tickets): add operation ids

--- services/tickets/infrastructure/transport/http/handler.go
+++ services/tickets/infrastructure/transport/http/handler.go
@@ -20,32 +20,32 @@ func NewHandler(healthChecker HealthChecker) *Handler {
 	return &Handler{healthChecker: healthChecker}
 }
 
-func (h *Handler) GetHealthz(ctx context.Context, _ server.GetHealthzRequestObject) (server.GetHealthzResponseObject, error) {
+func (h *Handler) GetHealth(ctx context.Context, _ server.GetHealthRequestObject) (server.GetHealthResponseObject, error) {
 	if err := h.healthChecker.Check(ctx); err != nil {
 		return nil, err
 	}
 
-	return server.GetHealthz200Response{}, nil
+	return server.GetHealth200Response{}, nil
 }
 
-func (h *Handler) PostInternalV1TicketIssue(context.Context, server.PostInternalV1TicketIssueRequestObject) (server.PostInternalV1TicketIssueResponseObject, error) {
-	return server.PostInternalV1TicketIssue500JSONResponse{InternalErrorJSONResponse: internalError()}, nil
+func (h *Handler) IssueTicket(context.Context, server.IssueTicketRequestObject) (server.IssueTicketResponseObject, error) {
+	return server.IssueTicket500JSONResponse{InternalErrorJSONResponse: internalError()}, nil
 }
 
-func (h *Handler) GetV1TicketList(context.Context, server.GetV1TicketListRequestObject) (server.GetV1TicketListResponseObject, error) {
-	return server.GetV1TicketList500JSONResponse{InternalErrorJSONResponse: internalError()}, nil
+func (h *Handler) ListTickets(context.Context, server.ListTicketsRequestObject) (server.ListTicketsResponseObject, error) {
+	return server.ListTickets500JSONResponse{InternalErrorJSONResponse: internalError()}, nil
 }
 
-func (h *Handler) GetV1TicketTicketId(context.Context, server.GetV1TicketTicketIdRequestObject) (server.GetV1TicketTicketIdResponseObject, error) {
-	return server.GetV1TicketTicketId500JSONResponse{InternalErrorJSONResponse: internalError()}, nil
+func (h *Handler) GetTicket(context.Context, server.GetTicketRequestObject) (server.GetTicketResponseObject, error) {
+	return server.GetTicket500JSONResponse{InternalErrorJSONResponse: internalError()}, nil
 }
 
-func (h *Handler) PostV1TicketTicketIdActivate(context.Context, server.PostV1TicketTicketIdActivateRequestObject) (server.PostV1TicketTicketIdActivateResponseObject, error) {
-	return server.PostV1TicketTicketIdActivate500JSONResponse{InternalErrorJSONResponse: internalError()}, nil
+func (h *Handler) ActivateTicket(context.Context, server.ActivateTicketRequestObject) (server.ActivateTicketResponseObject, error) {
+	return server.ActivateTicket500JSONResponse{InternalErrorJSONResponse: internalError()}, nil
 }
 
-func (h *Handler) PostV1TicketTicketIdDecline(context.Context, server.PostV1TicketTicketIdDeclineRequestObject) (server.PostV1TicketTicketIdDeclineResponseObject, error) {
-	return server.PostV1TicketTicketIdDecline500JSONResponse{InternalErrorJSONResponse: internalError()}, nil
+func (h *Handler) DeclineTicket(context.Context, server.DeclineTicketRequestObject) (server.DeclineTicketResponseObject, error) {
+	return server.DeclineTicket500JSONResponse{InternalErrorJSONResponse: internalError()}, nil
 }
 
 func internalError() server.InternalErrorJSONResponse {
